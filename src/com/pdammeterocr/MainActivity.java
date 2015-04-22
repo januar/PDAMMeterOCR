e61d5467955a0488becefc08f91958d8b57d03d0
januar <januar.srt@gmail.com>
2015-04-22 20:21:32 +0700

    UI Design

--- src/com/pdammeterocr/MainActivity.java
+++ src/com/pdammeterocr/MainActivity.java
@@ -3,6 +3,7 @@ package com.pdammeterocr;
 import java.io.File;
 
 import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
@@ -14,7 +15,7 @@ import android.view.MenuItem;
 import android.view.View;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
